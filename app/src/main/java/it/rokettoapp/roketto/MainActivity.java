471a45f62fc8f75b5ba80a69080b5bf78d0ba172
Samuele Simone <samuelesimone01@gmail.com>
2021-11-20 14:20:28 +0100

    Added bottom nav and fragments skeleton

--- app/src/main/java/it/rokettoapp/roketto/MainActivity.java
+++ app/src/main/java/it/rokettoapp/roketto/MainActivity.java
@@ -1,14 +1,45 @@
 package it.rokettoapp.roketto;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FragmentHome()).commit();
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch(item.getItemId()){
+                    case R.id.itemHome:
+                        fragment = new FragmentHome();
+                        break;
+                    case R.id.itemNews:
+                        fragment = new FragmentNews();
+                        break;
+                    case R.id.itemFavorites:
+                        fragment = new FragmentFavorites();
+                        break;
+                    case R.id.itemSettings:
+                        fragment = new FragmentSettings();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
+                return true;
+            }
+        });
     }
 }
