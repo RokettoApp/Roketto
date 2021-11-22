f23bac42207df9bb91cafe9540ceb2781021175c
Khalil Mohamed Khalil <k.mohamedkhalil@campus.unimib.it>
2021-11-22 15:39:17 +0100

    Aggiunta recycleView degli eventi

--- app/src/main/java/it/rokettoapp/roketto/FragmentHome.java
+++ app/src/main/java/it/rokettoapp/roketto/FragmentHome.java
@@ -8,13 +8,45 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import it.rokettoapp.roketto.adapter.RecyclerViewAdapterEvents;
+import it.rokettoapp.roketto.spaceEvents.SpaceEvents;
 
 public class FragmentHome extends Fragment {
+
+    private List<SpaceEvents> mEvents;
+
+    public FragmentHome() {
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mEvents = new ArrayList<>();
+        mEvents.add(new SpaceEvents("Apollo 1", "prova 1", new Date(1637570235000L)));
+        mEvents.add(new SpaceEvents("Apollo 2", "prova 2", new Date(1537570235000L)));
+        mEvents.add(new SpaceEvents("Apollo 3", "prova 3", new Date(1437570235000L)));
+        mEvents.add(new SpaceEvents("Apollo 4", "prova 4", new Date(1337570235000L)));
+        mEvents.add(new SpaceEvents("Apollo 5", "prova 5", new Date(127570235000L)));
+    }
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_home,container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home,container, false);
+
+        RecyclerView myrv = (RecyclerView) rootView.findViewById(R.id.rvEvents);
+        myrv.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+
+        RecyclerViewAdapterEvents myAdapter = new RecyclerViewAdapterEvents(getContext(), mEvents);
+        myrv.setAdapter(myAdapter);
         return rootView;
     }
 }
---
+++ app/src/main/java/it/rokettoapp/roketto/adapter/RecyclerViewAdapterEvents.java
@@ -0,0 +1,66 @@
+package it.rokettoapp.roketto.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import it.rokettoapp.roketto.R;
+import it.rokettoapp.roketto.spaceEvents.SpaceEvents;
+
+public class RecyclerViewAdapterEvents extends RecyclerView.Adapter<RecyclerViewAdapterEvents.MyViewHolderEvents> {
+
+    private List<SpaceEvents> mEvents;
+    private Context mContext;
+
+    public RecyclerViewAdapterEvents(Context mContext, List<SpaceEvents> mEvents) {
+        this.mContext = mContext;
+        this.mEvents = mEvents;
+    }
+
+    @Override
+    public MyViewHolderEvents onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view ;
+        LayoutInflater mInflater = LayoutInflater.from(mContext);
+        view = mInflater.inflate(R.layout.recycleview_row_events,parent,false);
+        return new MyViewHolderEvents(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolderEvents holder, final int position) {
+
+        holder.event_title.setText(mEvents.get(position).getTitle());
+        holder.event_d.setText(mEvents.get(position).getDescription());
+        holder.eventDate.setText(mEvents.get(position).getDepartureDate());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mEvents.size();
+    }
+
+    public static class MyViewHolderEvents extends RecyclerView.ViewHolder {
+
+        public TextView event_title;
+        public TextView event_d;
+        public TextView eventDate;
+        CardView cardView;
+
+        public MyViewHolderEvents(View itemView) {
+            super(itemView);
+            event_title = (TextView) itemView.findViewById(R.id.event_title);
+            event_d = (TextView) itemView.findViewById(R.id.event_d);
+            eventDate = (TextView) itemView.findViewById(R.id.eventDate);
+            cardView = (CardView) itemView.findViewById(R.id.event_card);
+        }
+    }
+
+
+}
