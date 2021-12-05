d3ebf1dff1b7f90ffa7a2bb792c2aac466dae2c9
Khalil Mohamed Khalil <k.mohamedkhalil@campus.unimib.it>
2021-12-05 13:27:54 +0100

    Implemented onClickListener on the cardView of the RecyclerViewAstronauts

--- app/src/main/java/it/rokettoapp/roketto/adapter/RecyclerViewAdapterAstro.java
+++ app/src/main/java/it/rokettoapp/roketto/adapter/RecyclerViewAdapterAstro.java
@@ -1,6 +1,7 @@
 package it.rokettoapp.roketto.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,8 @@ import java.util.List;
 
 import it.rokettoapp.roketto.R;
 import it.rokettoapp.roketto.model.Astronaut;
+import it.rokettoapp.roketto.ui.AstroDetailActivity;
+import it.rokettoapp.roketto.ui.EventDetailActivity;
 
 public class RecyclerViewAdapterAstro extends RecyclerView.Adapter<RecyclerViewAdapterAstro.MyViewHolderAstro> {
 
@@ -30,7 +33,17 @@ public class RecyclerViewAdapterAstro extends RecyclerView.Adapter<RecyclerViewA
         View view ;
         LayoutInflater mInflater = LayoutInflater.from(mContext);
         view = mInflater.inflate(R.layout.recycler_astronauts_item,parent,false);
-        return new MyViewHolderAstro(view);
+
+        MyViewHolderAstro mHolder = new MyViewHolderAstro(view, new MyClickListener() {
+            @Override
+            public void onClick(int p) {
+                Intent intent = new Intent(mContext, AstroDetailActivity.class);
+                intent.putExtra("Astronaut", mAstro.get(p));
+                mContext.startActivity(intent);
+            }
+        });
+
+        return mHolder;
     }
 
     @Override
@@ -44,16 +57,31 @@ public class RecyclerViewAdapterAstro extends RecyclerView.Adapter<RecyclerViewA
         return mAstro.size();
     }
 
-    public static class MyViewHolderAstro extends RecyclerView.ViewHolder {
+    public static class MyViewHolderAstro extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         public TextView astro_name;
         CardView cardView;
 
-        public MyViewHolderAstro(View itemView) {
+        MyClickListener listener;
+
+        public MyViewHolderAstro(View itemView, MyClickListener listener) {
             super(itemView);
             astro_name = (TextView) itemView.findViewById(R.id.astronautsName);
             cardView = (CardView) itemView.findViewById(R.id.astro_card);
+
+            this.listener = listener;
+
+            cardView.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View v) {
+            listener.onClick(this.getLayoutPosition());
+        }
+    }
+
+    public interface MyClickListener{
+        void onClick(int p);
     }
 
 
