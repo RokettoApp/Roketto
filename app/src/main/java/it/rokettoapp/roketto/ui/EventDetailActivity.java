8be56b7d72ab9a35493569c98118f375a43953f4
Khalil Mohamed Khalil <k.mohamedkhalil@campus.unimib.it>
2021-12-14 15:48:16 +0100

    Added FrameLayout and implemented glide in order to visualize Events' image

--- app/src/main/java/it/rokettoapp/roketto/ui/EventDetailActivity.java
+++ app/src/main/java/it/rokettoapp/roketto/ui/EventDetailActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import it.rokettoapp.roketto.R;
 import it.rokettoapp.roketto.model.Event;
@@ -21,6 +24,9 @@ public class EventDetailActivity extends AppCompatActivity {
         Event mEvent = (Event) getIntent().getSerializableExtra("Event");
         ImageButton back = (ImageButton)findViewById(R.id.backButtonEvent);
         TextView eventDescription = (TextView) findViewById(R.id.eventDetailDescription);
+        ImageView mEventImage = (ImageView) findViewById(R.id.imageEvent);
+
+        Glide.with(this).load(mEvent.getFeatureImage()).into(mEventImage);
 
         back.setOnClickListener(v -> onBackPressed());
 
