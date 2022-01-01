23620f6cb8de47ae17b261b02cd72a73d943d8a8
Davide Costantini <d.costantini6@campus.unimib.it>
2022-01-01 19:39:38 +0100

    Added favourite events

--- app/src/main/java/it/rokettoapp/roketto/model/User.java
+++ app/src/main/java/it/rokettoapp/roketto/model/User.java
@@ -1,13 +1,31 @@
 package it.rokettoapp.roketto.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
 
     private String id;
     private String email;
+    private List<Integer> favouriteEvents;
+
+    public User() {
+
+        // Required by Firebase
+    }
 
     public User(String id, String email) {
+
         this.id = id;
         this.email = email;
+        favouriteEvents = new ArrayList<>();
+    }
+
+    public User(String id, String email, List<Integer> eventIds) {
+
+        this.id = id;
+        this.email = email;
+        this.favouriteEvents = eventIds;
     }
 
     public String getId() {
@@ -28,5 +46,25 @@ public class User {
     public void setEmail(String email) {
 
         this.email = email;
+    }
+
+    public List<Integer> getFavouriteEvents() {
+
+        return favouriteEvents;
+    }
+
+    public void setFavouriteEvents(List<Integer> eventIds) {
+
+        this.favouriteEvents = eventIds;
+    }
+
+    public void addFavouriteEvent(Integer eventId) {
+
+        favouriteEvents.add(eventId);
+    }
+
+    public void removeFavouriteEvent(Integer eventId) {
+
+        favouriteEvents.remove(eventId);
     }
 }
