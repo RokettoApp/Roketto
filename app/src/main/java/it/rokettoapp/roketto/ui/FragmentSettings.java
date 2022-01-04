b32742b69d5006457cc9f0cdfc4bf8ddb5c8a9d9
Davide Costantini <d.costantini6@campus.unimib.it>
2022-01-04 18:44:59 +0100

    Added layout to FragmentSettings

--- app/src/main/java/it/rokettoapp/roketto/ui/FragmentSettings.java
+++ app/src/main/java/it/rokettoapp/roketto/ui/FragmentSettings.java
@@ -1,6 +1,8 @@
 package it.rokettoapp.roketto.ui;
 
+import android.app.Application;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +16,13 @@ import androidx.fragment.app.Fragment;
 import com.google.firebase.auth.FirebaseAuth;
 
 import it.rokettoapp.roketto.R;
+import it.rokettoapp.roketto.repository.AgencyRepository;
+import it.rokettoapp.roketto.repository.ArticleRepository;
+import it.rokettoapp.roketto.repository.AstronautRepository;
+import it.rokettoapp.roketto.repository.EventRepository;
+import it.rokettoapp.roketto.repository.ExpeditionRepository;
+import it.rokettoapp.roketto.repository.LaunchRepository;
+import it.rokettoapp.roketto.util.Constants;
 import it.rokettoapp.roketto.util.SharedPreferencesProvider;
 
 public class FragmentSettings extends Fragment {
@@ -30,16 +39,68 @@ public class FragmentSettings extends Fragment {
                              @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
-        FirebaseAuth.getInstance().signOut();
-        Button button = view.findViewById(R.id.logoutButton);
-        button.setOnClickListener(view1 -> {
+        Button logoutButton = view.findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(view1 -> {
 
+            FirebaseAuth.getInstance().signOut();
             SharedPreferencesProvider sharedPreferencesProvider =
                     new SharedPreferencesProvider(requireActivity().getApplication());
             sharedPreferencesProvider.deleteAll();
             startActivity(new Intent(requireActivity(), AuthenticationActivity.class));
             requireActivity().finish();
         });
+
+        Button clearCacheButton = view.findViewById(R.id.clearCacheButton);
+        clearCacheButton.setOnClickListener(view1 -> {
+
+            Application application = requireActivity().getApplication();
+            AgencyRepository agencyRepository = new AgencyRepository(application);
+            agencyRepository.clearAgencies();
+            ArticleRepository articleRepository = new ArticleRepository(application);
+            articleRepository.clearArticles();
+            AstronautRepository astronautRepository = new AstronautRepository(application);
+            astronautRepository.clearAstronauts();
+//            DockingEventRepository dockingEventRepository = new DockingEventRepository(application);
+//            dockingEventRepository.clearDockingEvents();
+            EventRepository eventRepository = new EventRepository(application);
+            eventRepository.clearEvents();
+            ExpeditionRepository expeditionRepository = new ExpeditionRepository(application);
+            expeditionRepository.clearExpeditions();
+            LaunchRepository launchRepository = new LaunchRepository(application);
+            launchRepository.clearLaunches();
+//            LauncherRepository launcherRepository = new LauncherRepository(application);
+//            launcherRepository.clearLaunchers();
+//            LocationRepository locationRepository = new LocationRepository(application);
+//            locationRepository.clearLocations();
+//            PadRepository padRepository = new PadRepository(application);
+//            padRepository.clearPads();
+//            ProgramRepository programRepository = new ProgramRepository(application);
+//            programRepository.clearPrograms();
+//            SpacecraftFlightRepository spacecraftFlightRepository =
+//                    new SpacecraftFlightRepository(application);
+//            spacecraftFlightRepository.clearSpacecraftFlight();
+//            SpacecraftRepository spacecraftRepository = new SpacecraftRepository(application);
+//            spacecraftRepository.clearSpacecrafts();
+//            SpaceStationRepository spaceStationRepository = new SpaceStationRepository(application);
+//            spaceStationRepository.clearSpaceStations();
+        });
+
+        Button aboutLaunchApiButton = view.findViewById(R.id.aboutLaunchApiButton);
+        aboutLaunchApiButton.setOnClickListener(view1 -> {
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(Constants.TSD_URL));
+            startActivity(intent);
+        });
+
+        Button aboutNewsApiButton = view.findViewById(R.id.aboutNewsApiButton);
+        aboutNewsApiButton.setOnClickListener(view1 -> {
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(Constants.SFN_URL));
+            startActivity(intent);
+        });
+
         return view;
     }
 }
