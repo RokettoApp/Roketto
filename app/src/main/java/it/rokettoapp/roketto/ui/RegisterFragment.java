0f7e6393b212e32675435408281e8742cf0cfd8e
Davide Costantini <d.costantini6@campus.unimib.it>
2022-01-22 12:48:21 +0100

    Updated login and register layouts

--- app/src/main/java/it/rokettoapp/roketto/ui/RegisterFragment.java
+++ app/src/main/java/it/rokettoapp/roketto/ui/RegisterFragment.java
@@ -1,16 +1,17 @@
 package it.rokettoapp.roketto.ui;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.Navigation;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -32,26 +33,37 @@ public class RegisterFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_register, container, false);
 
-        final EditText editTextEmail = view.findViewById(R.id.email);
-        final EditText editTextPassword = view.findViewById(R.id.password);
+        final EditText editTextEmail = view.findViewById(R.id.editEmail);
+        final EditText editTextPassword = view.findViewById(R.id.editPassword);
+        final EditText editTextConfirmPassword = view.findViewById(R.id.editConfirmPassword);
 
-        final Button buttonRegister = view.findViewById(R.id.register_button);
+        final Button buttonRegister = view.findViewById(R.id.btnSigninEmail);
         buttonRegister.setOnClickListener(view1 -> {
 
             String email = editTextEmail.getText().toString();
             String password = editTextPassword.getText().toString();
-            mUserViewModel.signUpWithEmail(email, password)
-                    .observe(getViewLifecycleOwner(), authenticationResponse -> {
+            String confirmPassword = editTextConfirmPassword.getText().toString();
+            if (password.equals(confirmPassword)) {
+                mUserViewModel.signUpWithEmail(email, password)
+                        .observe(getViewLifecycleOwner(), authenticationResponse -> {
 
-                if (authenticationResponse != null) {
-                    if (authenticationResponse.isSuccess()) {
-                        Navigation.findNavController(view1).navigate(R.id.mainActivity);
-                    } else {
-                        updateUIForFailure(authenticationResponse.getMessage());
-                    }
-                }
-            });
+                            if (authenticationResponse != null) {
+                                if (authenticationResponse.isSuccess()) {
+                                    Navigation.findNavController(view1).navigate(R.id.mainActivity);
+                                } else {
+                                    updateUIForFailure(authenticationResponse.getMessage());
+                                }
+                            }
+                        });
+            } else {
+                showError(getString(R.string.password_error));
+            }
         });
+
+        final TextView textViewLogIn = view.findViewById(R.id.linkSignup2);
+        textViewLogIn.setOnClickListener(v ->
+                Navigation.findNavController(v)
+                        .navigate(R.id.action_registerFragment_to_loginFragment));
 
         return view;
     }
@@ -61,5 +73,11 @@ public class RegisterFragment extends Fragment {
         Snackbar.make(requireActivity().findViewById(android.R.id.content), message,
                 Snackbar.LENGTH_SHORT).show();
         mUserViewModel.clear();
+    }
+
+    private void showError(String errorMessage) {
+
+        Toast toast = Toast.makeText(getContext(), errorMessage, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
